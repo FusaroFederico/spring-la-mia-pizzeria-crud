ea80fc814913e6cb205c6ccfda26a98d0f310645
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-08-28 16:00:47 +0200

    Create PizzaController.java

---
+++ project-pizzeria/spring-la-mia-pizzeria-crud/src/main/java/com/example/demo/pizzeria/controller/PizzaController.java
@@ -0,0 +1,36 @@
+package com.example.demo.pizzeria.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.pizzeria.model.Pizza;
+import com.example.demo.pizzeria.repo.PizzaRepository;
+
+@Controller
+@RequestMapping("/pizzas")
+public class PizzaController {
+	
+	// repository field con autowired per d.i.
+	@Autowired
+	private PizzaRepository repo;
+	
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("title", "Menù");
+		
+		// prendo i dati da consegnare a /pizzas/index
+		List<Pizza> pizzas = repo.findAll();
+		// li inserisco nel model
+		model.addAttribute("pizzas", pizzas);
+		
+		model.addAttribute("curPage", "pizzas");
+		return "/pizzas/index";
+	}
+}
