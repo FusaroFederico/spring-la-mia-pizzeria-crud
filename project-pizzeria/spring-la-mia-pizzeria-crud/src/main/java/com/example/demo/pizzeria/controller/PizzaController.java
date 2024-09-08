fe579be37a455359a150fe24f1ee2c3718f2df21
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-09-08 18:25:01 +0200

    Update PizzaController.java
    Add @PostMapping store() method.

--- project-pizzeria/spring-la-mia-pizzeria-crud/src/main/java/com/example/demo/pizzeria/controller/PizzaController.java
+++ project-pizzeria/spring-la-mia-pizzeria-crud/src/main/java/com/example/demo/pizzeria/controller/PizzaController.java
@@ -6,13 +6,19 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.pizzeria.model.Pizza;
 import com.example.demo.pizzeria.repo.PizzaRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pizzas")
@@ -65,4 +71,19 @@ public class PizzaController {
 		return "/pizzas/create";
 	}
 	
+	@PostMapping ("/create")
+	public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, 
+					     BindingResult bindingResult,
+					     RedirectAttributes redirectAttributes) {
+		
+		if (bindingResult.hasErrors()) {
+			return "/pizzas/create";
+		}
+		
+		repo.save(formPizza);
+		
+		redirectAttributes.addFlashAttribute("successMessage", "La pizza '" + formPizza.getName() + "' è stata aggiunta al menù.");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		return "redirect:/pizzas";
+	}
 }
